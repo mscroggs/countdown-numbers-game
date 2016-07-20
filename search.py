5dc7118db3324933bb31ca623dcf67e008ce86fe
MScroggs <matthew.w.scroggs@gmail.com>
2016-07-20 16:15:52 +0100

    Don't have to use all numbers

--- search.py
+++ search.py
@@ -7,9 +7,18 @@ def possible(numbers):
     for nums in permutations([str(i) for i in numbers]):
         for signs in product(["+","-","/","*"], repeat=5):
             p = []
+            p.append(nums[4]+signs[4]+nums[5])
+            p.append(nums[3]+signs[3]+"("+nums[4]+signs[4]+nums[5]+")")
+            p.append(nums[2]+signs[2]+"("+nums[3]+signs[3]+"("+nums[4]+signs[4]+nums[5]+"))")
+            p.append(nums[1]+signs[1]+"("+nums[2]+signs[2]+"("+nums[3]+signs[3]+"("+nums[4]+signs[4]+nums[5]+")))")
             p.append(nums[0]+signs[0]+"("+nums[1]+signs[1]+"("+nums[2]+signs[2]+"("+nums[3]+signs[3]+"("+nums[4]+signs[4]+nums[5]+"))))")
+
+            p.append("("+nums[2]+signs[2]+nums[3]+")"+signs[3]+"("+nums[4]+signs[4]+nums[5]+")")
+            p.append(nums[1]+signs[1]+"(("+nums[2]+signs[2]+nums[3]+")"+signs[3]+"("+nums[4]+signs[4]+nums[5]+"))")
             p.append(nums[0]+signs[0]+"("+nums[1]+signs[1]+"(("+nums[2]+signs[2]+nums[3]+")"+signs[3]+"("+nums[4]+signs[4]+nums[5]+")))")
+
             p.append("("+nums[0]+signs[0]+nums[1]+")"+signs[1]+"(("+nums[2]+signs[2]+nums[3]+")"+signs[3]+"("+nums[4]+signs[4]+nums[5]+"))")
+
             for m in p:
                 try:
                     e = eval(m)
