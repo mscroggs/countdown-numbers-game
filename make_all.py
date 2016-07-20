9e7a85a42463d4ccaddaa0d46fd7dbecee678d47
MScroggs <matthew.w.scroggs@gmail.com>
2016-07-20 17:00:02 +0100

    Added code to make countdown-combinations file

--- make_all.py
+++ make_all.py
@@ -11,16 +11,28 @@ ls += [25,50,75,100]
 
 count = 0
 
+def format(stuff):
+    stuff = str(stuff)
+    return " "*(3-len(stuff)) + stuff
+
+with open("output/countdown-combinations","w") as f:
+    f.write("Numbers             Number of possible totals\n")
+
 for n in range(5):
     for large in combinations([25,50,75,100], n):
         for small in combinations_with_replacement(range(1,11), 6-n):
             try:
                 assert max([len([s for s in small if s==i]) for i in range(1,11)])<=2
                 made = possible(small+large)
-                if len([m for m in made if made[m] is None]) == 0:
+                strnum = " ".join([str(i) for i in small+large])
+                possibles = len([m for m in made if made[m] is not None])
+                with open("output/countdown-combinations","a") as f:
+                    f.write(strnum + " "*(20-len(strnum)) + format(possibles)+"/899")
+                    f.write("\n")
+                if possibles == 899:
                     count += 0
                     print(small+large)
             except AssertionError:
                 pass
-                
+            
 print(str(count)+" combinations allow every total to be made")
