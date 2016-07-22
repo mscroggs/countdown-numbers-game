d0cd18be5cc35d5a59b798c74175185bfc986ed7
MScroggs <matthew.w.scroggs@gmail.com>
2016-07-22 11:13:44 +0100

    Added more output files

--- save.py
+++ save.py
@@ -8,7 +8,19 @@ def format(j):
         return "-"
     return j
 
-made = possible(numbers)
+for numbers in [[5,6,8,9,10,100],
+    [5,6,7,8,10,100],
+    [4,6,7,8,9,100],
+    [3,6,7,8,10,100],
+    [3,5,7,8,9,100],
+    [2,5,6,8,9,100],
+    [2,6,7,8,9,100],
+    [5,6,8,9,75,100],
+    [3,6,8,10,75,100],
+    [2,6,9,10,75,100]]:
+    print numbers
 
-with open("output/"+"-".join([str(i) for i in numbers])+".txt","w") as f:
-    f.write("\n".join([str(i)+" "+format(j) for i,j in made.items()]))
+    made = possible(numbers)
+
+    with open("output/"+"-".join([str(i) for i in numbers])+".txt","w") as f:
+        f.write("\n".join([str(i)+" "+format(j) for i,j in made.items()]))
